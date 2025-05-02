3c0dff31c5b71cc59a058c06df69dc821e79be4d
brunanschultz <b-ns-98@hotmail.com>
2025-05-02 12:32:07 -0300

    Add SQL Create DB e Tabela Usuarios

--- src/main/database/Script.sql
+++ src/main/database/Script.sql
@@ -1,9 +1,14 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
- */
-/**
- * Author:  bruna
- * Created: 2 de mai. de 2025
- */
+CREATE DATABASE poo_2025;
+GO
+USE poo_2025;
+GO
 
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
+GO
+
+INSERT INTO usuarios (email, senha) VALUES 
+('bruna','123');
